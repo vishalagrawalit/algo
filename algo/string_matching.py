2c22413157b5b62a8516a1a52973be1f40b6b1bc
vishvishu <vishvishu14@gmail.com>
2017-12-09 10:50:19 +0530

    Rabin-Karp Algorithm. Time Complexity- O((n-m+1)*m)

--- algo/string_matching.py
+++ algo/string_matching.py
@@ -43,3 +43,51 @@ def naive_times(text, pattern, length_text, length_pattern):
         if x-i==length_pattern:
             count+=1
     return count
+
+# It returns the number of times the pattern is present in the text.
+def rabin_karp(text, pattern, length_text, length_pattern):
+    mod = 11
+    if length_text<length_pattern:
+        return 0
+    p = int(psttern)%mod
+    for i in range(length_text - length_pattern):
+        if int(text[i:i+length_pattern])%mod == p:
+            flag = 1
+            for j in range(length_pattern):
+                if text[i+j] != pattern[j]:
+                    flag = -1
+                    break
+            if flag==1:
+                return i
+    return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
